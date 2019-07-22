2f938ce14f59b27b652de57596f2f12c03692b16
SgrAlpha <SgrAlpha@users.noreply.github.com>
2019-07-22 10:07:31 +0800

    Update one test

--- engine/src/test/java/io/sgr/telegram/bot/engine/utils/ExponentialBackOffTest.java
+++ engine/src/test/java/io/sgr/telegram/bot/engine/utils/ExponentialBackOffTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2017-2019 SgrAlpha
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package io.sgr.telegram.bot.engine.utils;
 
 import static io.sgr.telegram.bot.engine.utils.ExponentialBackOff.DEFAULT_INITIAL_INTERVAL_IN_MILLI;
@@ -26,16 +43,19 @@ public class ExponentialBackOffTest {
         assertEquals(currentInterval, nextBackOff, currentInterval * backOff.getRandomizationFactor());
         long lastInterval = currentInterval;
 
-        int round = new Random().nextInt(100) + 1;
+        int round = new Random().nextInt(100) + 100;  // At least 100 rounds.
         while (round > 0) {
             currentInterval = backOff.getCurrentInterval();
             nextBackOff = backOff.getNextBackOffInMilli();
             if (lastInterval != backOff.getMaxIntervalInMilli()) {
-                assertTrue(String.format("Round %d, last interval is %d but next back off is %d", round, lastInterval, nextBackOff), nextBackOff > lastInterval);
+                assertTrue(String.format("Round %d, last interval is %d but next back off is %d", round, lastInterval, nextBackOff),
+                        nextBackOff > lastInterval);
             }
             double delta = currentInterval * backOff.getRandomizationFactor();
-            assertTrue(nextBackOff > currentInterval - delta);
-            assertTrue(nextBackOff < currentInterval + delta);
+            assertTrue(String.format("Next backoff %d is smaller than expected %d minus delta %f", nextBackOff, currentInterval, delta),
+                    nextBackOff >= currentInterval - delta + 1);
+            assertTrue(String.format("Next backoff %d is larger than expected %d plus delta %f", nextBackOff, currentInterval, delta),
+                    nextBackOff <= currentInterval + delta + 1);
             lastInterval = currentInterval;
             round--;
         }
